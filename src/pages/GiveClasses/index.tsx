0e3f847a2a8a8ed09f8fe0bda08262a6d9bdb297
Thulio <thulio.alves@gmail.com>
2020-08-08 19:41:34 -0300

    Pagina de "Dar aulas"

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,8 +1,36 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
+import styles from "./styles";
 
 function GiveClass() {
-  return <View />;
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
+  );
 }
 
 export default GiveClass;
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,5 +1,4 @@
 import { StyleSheet } from "react-native";
-import { Poppins_400Regular } from "@expo-google-fonts/poppins";
 
 const styles = StyleSheet.create({
   container: {
